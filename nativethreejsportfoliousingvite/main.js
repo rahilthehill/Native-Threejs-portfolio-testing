ea0e10997422dc0efb47887797a3ce62aff3248d
rahilthehill <69742190+rahilthehill@users.noreply.github.com>
2023-02-26 14:17:34 +0800

    kjh

---
+++ nativethreejsportfoliousingvite/main.js
@@ -0,0 +1,61 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+// Set up the scene, camera, and renderer
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// Set up the controls
+const controls = new OrbitControls(camera, renderer.domElement);
+camera.position.z = 5;
+
+// Add an object to the scene
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+// Set up lighting
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+scene.add(directionalLight);
+
+// Resize the renderer and camera when the window is resized
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+window.addEventListener('resize', onWindowResize);
+
+// Render the scene
+function animate() {
+  requestAnimationFrame(animate);
+  
+  // Calculate the mouse position in 2D space
+  const mouse = new THREE.Vector2();
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+  
+  // Project the mouse position into 3D space
+  const raycaster = new THREE.Raycaster();
+  raycaster.setFromCamera(mouse, camera);
+  const intersects = raycaster.intersectObjects(scene.children);
+  if (intersects.length > 0) {
+    const { x, y, z } = intersects[0].point;
+    cube.position.set(x, y);
+  }
+  
+  renderer.render(scene, camera);
+}
+renderer.domElement.addEventListener('mousemove', animate);
+
+// Set up CSS to remove white border around canvas
+document.body.style.margin = 0;
+document.body.style.padding = 0;
+document.documentElement.style.margin = 0;
+document.documentElement.style.padding = 0;
